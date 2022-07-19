79535273852aa4c22e79f7e736caef5a2fd630c5
Praveen Pushkar <praveenpushkar140@gmail.com>
2022-07-19 23:40:22 +0530

    Updates

---
+++ precache-manifest.87e9d174eb9f0c5b63bc191a37b996b9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deb0066ebc50e1111f95152ccc1553e4",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "032c0892d6383f4037f5",
+    "url": "/react/static/js/2.8a3589a0.chunk.js"
+  },
+  {
+    "revision": "bedd79a7d05f988836b3",
+    "url": "/react/static/js/main.8de51857.chunk.js"
+  },
+  {
+    "revision": "f3f3f1408280dca43787",
+    "url": "/react/static/js/runtime-main.503b04fd.js"
+  }
+]);
